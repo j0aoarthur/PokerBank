9c0f6f4028c6d6421187b85371bddf99c3901a8f
joaoarthur <arthur1081@hotmail.com>
2025-05-06 14:15:16 -0300

    feat: Initial structure

---
+++ src/main/java/com/j0aoarthur/pokerbank/PokerBankApplication.java
@@ -0,0 +1,13 @@
+package com.j0aoarthur.pokerbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokerBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/j0aoarthur/pokerbank/PokerBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.j0aoarthur.pokerbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokerBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
