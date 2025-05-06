c1cc060b31d90476cd3f4cef6b9c161f7bc48336
joaoarthur <arthur1081@hotmail.com>
2025-05-06 14:17:19 -0300

    feat: Added ranking controller

---
+++ src/main/java/com/j0aoarthur/pokerbank/controllers/PlayerRankingController.java
@@ -0,0 +1,34 @@
+package com.j0aoarthur.pokerbank.controllers;
+
+import com.j0aoarthur.pokerbank.DTOs.response.PlayerRankingDTO;
+import com.j0aoarthur.pokerbank.entities.PlayerRanking;
+import com.j0aoarthur.pokerbank.services.PlayerRankingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/ranking")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class PlayerRankingController {
+
+    @Autowired
+    private PlayerRankingService playerRankingService;
+
+    @GetMapping
+    public ResponseEntity getRanking() {
+        List<PlayerRanking> ranking = playerRankingService.getPlayerRankings();
+
+        return ResponseEntity.ok(ranking.stream().map(PlayerRankingDTO::new).toList());
+    }
+
+    @GetMapping("/top")
+    public ResponseEntity getTopPlayers(@RequestParam(defaultValue = "4") int limit) {
+        List<PlayerRanking> topPlayers = playerRankingService.getTopPlayers(limit);
+        return ResponseEntity.ok(topPlayers.stream().map(PlayerRankingDTO::new).toList());
+    }
+
+
+}
