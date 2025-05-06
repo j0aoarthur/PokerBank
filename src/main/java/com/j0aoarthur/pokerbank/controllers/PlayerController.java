16fe3b8612a3517486f22b70d9d9123039c3b557
joaoarthur <arthur1081@hotmail.com>
2025-05-06 14:17:08 -0300

    feat: Added Player controller

---
+++ src/main/java/com/j0aoarthur/pokerbank/controllers/PlayerController.java
@@ -0,0 +1,34 @@
+package com.j0aoarthur.pokerbank.controllers;
+
+import com.j0aoarthur.pokerbank.DTOs.request.PlayerRequestDTO;
+import com.j0aoarthur.pokerbank.entities.Player;
+import com.j0aoarthur.pokerbank.services.PlayerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/players")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class PlayerController {
+
+    @Autowired
+    private PlayerService playerService;
+
+    @PostMapping
+    public ResponseEntity<Player> createPlayer(@RequestBody PlayerRequestDTO playerDTO) {
+        Player createdPlayer = playerService.createPlayer(playerDTO);
+        return ResponseEntity.ok(createdPlayer);
+    }
+
+    @GetMapping
+    public ResponseEntity getAllPlayers() {
+        return ResponseEntity.ok(playerService.getAllPlayers());
+    }
+
+    @GetMapping("/not-in-game/{gameId}")
+    public ResponseEntity getPlayersNotInGame(@PathVariable Long gameId) {
+        return ResponseEntity.ok(playerService.getPlayersNotInGame(gameId));
+    }
+}
