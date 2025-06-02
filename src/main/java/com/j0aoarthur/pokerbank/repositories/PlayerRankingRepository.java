7d2a7d393d1df8b6d108ae6df2efee0473841b14
joaoarthur <arthur1081@hotmail.com>
2025-06-02 03:43:04 -0300

    refactor: Improved Ranking logic and queries

--- src/main/java/com/j0aoarthur/pokerbank/repositories/PlayerRankingRepository.java
+++ src/main/java/com/j0aoarthur/pokerbank/repositories/PlayerRankingRepository.java
@@ -3,8 +3,11 @@ package com.j0aoarthur.pokerbank.repositories;
 import com.j0aoarthur.pokerbank.entities.PlayerRanking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PlayerRankingRepository extends JpaRepository<PlayerRanking, Long> {
     Optional<PlayerRanking> findByPlayerId(Long playerId);
+
+    List<PlayerRanking> findAllByGamesPlayedAfter(Integer gamesPlayedAfter);
 }
