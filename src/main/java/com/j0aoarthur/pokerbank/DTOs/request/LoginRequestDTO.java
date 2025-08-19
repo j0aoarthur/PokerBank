a17f55ec708f98f523fc6f6a90590d4911814c01
joaoarthur <joao.oliveira@sitelbra.com.br>
2025-08-19 18:11:47 -0300

    feat: Add LoginRequestDTO for user authentication requests

---
+++ src/main/java/com/j0aoarthur/pokerbank/DTOs/request/LoginRequestDTO.java
@@ -0,0 +1,12 @@
+package com.j0aoarthur.pokerbank.DTOs.request;
+
+import jakarta.validation.constraints.NotNull;
+
+public record LoginRequestDTO(
+
+        @NotNull(message = "Nome de usuário é obrigatório")
+        String username,
+
+        @NotNull(message = "Senha é obrigatória")
+        String password
+) {}
