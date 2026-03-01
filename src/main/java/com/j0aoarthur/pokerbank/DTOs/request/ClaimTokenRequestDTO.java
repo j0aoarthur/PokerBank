cacae98bf6738a9b11beecb9cd4f49d174ee868f
joaoarthur <arthur1081@hotmail.com>
2026-02-28 22:07:13 -0300

    feat: add validation annotation for claimToken in ClaimTokenRequestDTO

--- src/main/java/com/j0aoarthur/pokerbank/DTOs/request/ClaimTokenRequestDTO.java
+++ src/main/java/com/j0aoarthur/pokerbank/DTOs/request/ClaimTokenRequestDTO.java
@@ -1,7 +1,10 @@
 package com.j0aoarthur.pokerbank.dtos.request;
 
+import jakarta.validation.constraints.NotNull;
+
 import java.util.UUID;
 
-public record ClaimTokenRequestDTO(UUID claimToken) {
-    
-}
+public record ClaimTokenRequestDTO(
+        @NotNull(message = "O token de reivindicação é obrigatório.")
+        UUID claimToken
+) {}
